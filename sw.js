f1589a132304b77463057ed3ab66bb7ab4378566
centiger <centiger2@gmail.com>
2026-07-09 06:08:19 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='cen-worship-explorer-v161-cachefix';
-const ASSETS=['./','./index.html','./style.css?v=1.6.1','./app.js?v=1.6.1','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+const CACHE='cen-worship-explorer-v17-compare';
+const ASSETS=['./','./index.html','./style.css?v=1.7','./app.js?v=1.7','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{ self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate',e=>{ e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
 self.addEventListener('fetch',e=>{
